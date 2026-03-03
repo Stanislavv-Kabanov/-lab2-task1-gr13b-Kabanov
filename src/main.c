26b840e2519cb674664e5b3df86dffc3c578fc8b
Stanislavv-Kabanov <stask040506@gmail.com>
2026-03-03 18:43:20 +0000

    создание функции main, создание и заполнение массива

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
-/* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main (void)
+int main (int args, char *argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	if (atgs != 2)
+	{
+	printf("Использование: %s <размер массива>\n",argv[0]);
+	printf("Пример: %s 10\n", argv[0]);
+	return 1;	
+	}
+
+	int n = atoi (argv[1]);
+
+	if(n <= 0)
+	{
+		printf("Ошибка: размер массива должен быть положительным числом\n");
+		return 1;
+	}
+
+	srand(time(NULL));
+
+	int arr[n];
+
+	for (int i = 0; i < n; i++) 
+		{
+				arr[i] = rand() % 100 + 1;  
+		}
+
+	return 0;
+
 }
