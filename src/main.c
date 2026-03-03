f6d771196bb2a95c9fc6417f658bc40606b90143
Stanislavv-Kabanov <stask040506@gmail.com>
2026-03-03 18:47:27 +0000

    Создана функция для нахождения суммы цифр числа sum_digits

--- src/main.c
+++ src/main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+int sum_digits(int num)
+{
+	int sum = 0;
+	num = abs(num);
+
+	while(num > 0)
+		{
+			sum = num % 10;
+			num /= 10;
+		}
+
+	return sum;
+}
+
 int main (int args, char *argv[])
 {
 	if (atgs != 2)
@@ -27,6 +41,13 @@ int main (int args, char *argv[])
 		{
 				arr[i] = rand() % 100 + 1;  
 		}
+
+	printf("Суммы цифр:      ");
+	for (int i = 0; i < n; i++) 
+	{
+			printf("%d ", sum_digits(arr[i]));
+	}
+	printf("\n");
 
 	return 0;
 
